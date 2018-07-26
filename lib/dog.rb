ac9925e7bed905ccfdf97806944f0337adaba732
aleajane <heyya.its.alea@gmail.com>
2018-07-26 20:34:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,12 @@ class Dog
   def initiative 
   end
     
-  #bark = puts "Woof!"  
-  #sit = puts "The Dog is sitting."
-    
+ def bark
+   puts "Woof!"
+ end 
+ 
+  def sit 
+    puts "The Dog is sitting."
+  end 
+  
 end 
